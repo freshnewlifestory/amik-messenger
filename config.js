2e0bacb72dc5ea4997d3c4dab801b329b5600f0f
freshnewlifestory <sakibgaming505@gmail.com>
2026-06-08 09:31:32 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 console.log("Config Loaded");
+
 const SUPABASE_URL = "https://bplfzbfuvwcdowtffacn.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_MsnBAwQNEywB1tmN8CXb6A_eTq-rVbX";
 
-const supabase = window.supabase.createClient(
+const { createClient } = supabase;
+
+const db = createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+
+console.log("Supabase Connected");
